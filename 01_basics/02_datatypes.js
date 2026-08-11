96ea55a02534a235122122fe96a51193b30bcb84
shasi4069 <shasi4069@gmail.com>
2026-08-11 10:07:52 +0530

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version 
+//alert(3+3) we are using nodejs not browser
+console.log("this is my java script learning journey")
+let name="shasi"
+let age=18
+let loggedIn=true
+console.log(name , age, loggedIn)
+
+//there are some data types----
+//number=2 to 52
+//bigint
+//string=""
+//boolean=true/false
+//null=standalone value
+//undefined=
+//symbol=unique
+
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);

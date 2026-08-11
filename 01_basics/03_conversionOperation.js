54d00f7c8cd361e44295069f780989b4b74549d7
shasi4069 <shasi4069@gmail.com>
2026-08-11 10:43:33 +0530

    conversion of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+// let score="33"
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"=33
+//"33abc"NaN(not a no.)
+//true=1; false=0
+
+let isLoggedIn=""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=true; 0= false
+//""=false
+//"shasi"=true

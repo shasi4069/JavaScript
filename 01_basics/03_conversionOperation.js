004227471f72cc540a69758da47b9673421b50c6
shasi4069 <shasi4069@gmail.com>
2026-08-11 13:05:51 +0530

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -9,10 +9,33 @@
 //"33abc"NaN(not a no.)
 //true=1; false=0
 
-let isLoggedIn=""
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn=""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 //1=true; 0= false
 //""=false
 //"shasi"=true
+
+// let value=5
+// let negValue = -value
+// console.log(negValue);
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3**4);
+// console.log(3%3);
+// console.log(3/3);
+// let str1="hello"
+// let str2=" shasi"
+// let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+"2"+2);
+// console.log(+true);
+let gamecounter=100
+gamecounter--;
+console.log(gamecounter);

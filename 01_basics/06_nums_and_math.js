3b91f6df339e5ad595e35fe4e91056a3a6c73d5d
shasi4069 <shasi4069@gmail.com>
2026-08-12 11:15:50 +0530

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,37 @@
+// const score =400
+// console.log(score);
+//  const balance=new Number(100)
+//  console.log(balance);
+ 
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+//  const othernumber=25.6776875
+//  console.log(othernumber.toPrecision(3));
+ 
+//  const hundreds=10000000
+//  console.log(hundreds.toLocaleString('en-IN'));
+ 
+//+++++++++++++Maths+++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.ceil(6.2));
+// console.log(Math.round(7.4));
+// console.log(Math.floor(5.8));
+// console.log(Math.min(2,3,4,5,6));
+// console.log(Math.max(2,5,6,7,8));
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()*10) +1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max-min+1))+min)
+
+
+
+
+
+
+

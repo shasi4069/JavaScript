0ec2ca3f09499d3ea5155f6a1cf80bfe1cb83b09
shasi4069 <shasi4069@gmail.com>
2026-08-12 10:02:59 +0530

    string overview

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,25 @@
+const name="shasi"
+const package="15L"
+// console.log(name + package + " received");
+console.log('hello my name is ${name} and my package is ${package}') ;
+
+const gamename=new String ('shasi')
+console.log(gamename[1]);
+console.log(gamename .length);
+console.log(gamename .endsWith());
+console.log(gamename .toUpperCase());
+console.log(gamename.charAt('3'));
+console.log(gamename.indexOf('a'));
+console.log(gamename.substring(0,3));
+console.log(gamename.slice(-5,6));
+
+const newstring="   hello   "
+console.log(newstring);
+console.log(newstring.trim());
+
+const url="https://shasi@google.com02"
+console.log(url.replace('02','_'));
+
+console.log(url.includes('shasi'));
+
+console.log(gamename.split('-'));
---
+++ 01_basics/dataType_Summary.js
@@ -0,0 +1,16 @@
+//stack (primitive data type)
+//heap(non promitive data type(reference type))
+
+let myyoutubename="shasi4069"
+let anothername=myyoutubename
+ anothername='codewithshasi'
+console.log(myyoutubename);
+console.log(anothername);
+
+let user1={
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+let user2=user1
+
+user2.email="shasi@google.com"

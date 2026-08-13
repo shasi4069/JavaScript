2d19638940f33c7af430b695a3617c3886e43d3f
shasi4069 <shasi4069@gmail.com>
2026-08-13 19:23:41 +0530

    methods of array

---
+++ 02_basics/02_array.js
@@ -0,0 +1,25 @@
+const marvel_heros=["thor","Ironman","spiderman"]
+const dc_heros=["superman","flash","batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allhero=marvel_heros.concat(dc_heros)
+// console.log(allhero);
+
+// const all_new_heros=[...marvel_heros,...dc_heros]
+// console.log(all_new_heros);
+
+// const another_array=[1,2,3,[4,5,6],6,[4,[3,8]]]
+// const real_another_arr=another_array.flat(Infinity)
+// console.log(real_another_arr);
+
+// console.log(Array.isArray("shasi"));
+// console.log(Array.from("shasi"));
+
+let A=100
+let B=222
+let c=723
+console.log(Array.of(A,B,c));

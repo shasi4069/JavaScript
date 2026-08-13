f6a8cbd9a85b3de2a77c0d8f1b9551acbb6e9240
shasi4069 <shasi4069@gmail.com>
2026-08-13 18:53:44 +0530

    Array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,40 @@
+//array
+const myArr=[1,2,3,4,5]
+// console.log(myArr[2]);
+// const myArr1=new Array(1,2,3,4,5)
+// console.log(myArr[4]);
+
+// const myHeros=["A","B"]
+// console.log(myHeros);
+
+//Arrat methods
+
+// myArr.push(6)
+// myArr.pop()
+// myArr.unshift(7)
+// myArr.shift()
+// console.log(myArr);
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(2));
+
+// const newArr=myArr.join()
+// console.log(newArr);
+
+// console.log(typeof newArr);
+
+//slice and splice
+console.log("A",myArr);
+const myn1=myArr.slice(1,3)
+console.log(myn1);
+console.log("B",myArr);
+const myn2=myArr.splice(1,3)
+console.log("C",myArr);
+
+console.log(myn2);
+
+
+
+
+
+
+

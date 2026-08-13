03c6ba783d07155fec31428c5b5465a7d6eb9ff7
shasi4069 <shasi4069@gmail.com>
2026-08-13 18:04:46 +0530

    fix: correct string interpolation syntax in greeting message

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -12,6 +12,18 @@
 
 // let mycreatedDate=new Date(2026,0,13 , 9,7)
 // console.log(mycreatedDate.toLocaleString());
-
+let mycreatedDate=new Date("01-14-2026")
 let myTimeStamp=Date.now()
-console.log(myTimeStamp);
+// console.log(myTimeStamp);
+// console.log(mycreatedDate.getTime());
+
+let newDate= new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
+
+console.log(newDate.toLocaleString('default',{weekday:"long"}))
+
+
+
+
